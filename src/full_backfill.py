041ac7bd6d6c9b570cb705dff45fbfd6d3158bd4
zcox10 <zach.s.cox@gmail.com>
2024-04-13 12:54:53 -0700

    2024-04-13 pt. 1

--- src/full_backfill.py
+++ src/full_backfill.py
@@ -1,3 +1,4 @@
+import time
 import pandas as pd
 from google.cloud import bigquery
 import plaid
@@ -5,6 +6,7 @@ from utils.bq_utils import BqUtils
 from utils.plaid_utils import PlaidUtils
 from sql.bq_table_schemas import BqTableSchemas
 
+# constants
 PLAID_CLIENT_ID = "65975384ab670e001c0aaf0d"
 # PLAID_SECRET="56e33c77237c8c9e45f5c066b8b2fa" #production
 PLAID_SECRET = "9294dd5ca4a5c99d90da56640f40e5"  # sandbox
@@ -19,39 +21,58 @@ PLAID_ACCESS_TOKENS = [
     "access-sandbox-f415798a-74af-4e7f-b504-dc075dc5889c",
 ]
 
-
+# initialize clients
 bq_client = bigquery.Client()
 bq = BqUtils(bq_client=bq_client)
 plaid_client = PlaidUtils(bq_client, PLAID_CLIENT_ID, PLAID_SECRET, PLAID_HOST)
 bq_tables = BqTableSchemas()
 
-plaid_cursors_bq = bq_tables.plaid_cursors_YYYYMMDD()
+### START HERE
+backfill = True
+offset_days = -1
 
+# only create new plaid_accounts table and plaid_cursors_YYYYMMDD table if starting with initial backfill
+if backfill:
+    print("STARTING HISTORICAL DATA PULL")
+    # Create a new plaid_accounts table to, upload all account info according to access_tokens provided
+    plaid_client.create_accounts_bq_table(
+        access_tokens=PLAID_ACCESS_TOKENS, plaid_country_codes=PLAID_COUNTRY_CODES, confirm=False
+    )
 
-# RUN THIS SHIT
-plaid_client.create_accounts_bq_table(
-    access_tokens=PLAID_ACCESS_TOKENS, plaid_country_codes=PLAID_COUNTRY_CODES, confirm=False
-)
-plaid_client.create_cursors_bq_table(confirm=False)
+    # Create a new plaid_cursors_YYYYMMDD table with access_token, item_id, and next_cursor
+    plaid_client.create_cursors_bq_table(offset_days=offset_days, confirm=False)
 
-# create empty cursor table
+else:
+    print("STARTING DAILY DATA PULL")
+
+# create empty temp cursor table to upload cursors to for the current run.
+# When job finishes running, this table will become the latest plaid_cursors_YYYYMMDD partitions
 plaid_client.create_temp_cursors_bq_table(confirm=False)
 
-# create empty transactions table
-plaid_client.create_empty_transactions_bq_table(confirm=False)
-plaid_client.create_empty_removed_bq_table(confirm=False)
+# create empty plaid_transactions_YYYYMMDD and plaid_removed_transactions_YYYYMMDD to upload transactions to
+plaid_client.create_empty_transactions_bq_table(offset_days=offset_days, confirm=False)
+plaid_client.create_empty_removed_bq_table(offset_days=offset_days, confirm=False)
 
 # grab latest cursors for each access token / item
 latest_cursors_df = plaid_client.get_latest_cursors()
 
-# Run get_transactions() to store added/modified transactions in transactions_df and removed transactions in removed_df
-# print(latest_cursors_df["access_token"][0])
-transactions_df, removed_df = plaid_client.get_transactions(
-    access_token=latest_cursors_df["access_token"][0],
-    item_id=latest_cursors_df["item_id"][0],
-    next_cursor=latest_cursors_df["next_cursor"][0],
-)
+# give time for tables to be created
+print("SLEEP 10 SEC TO WAIT FOR BQ TABLES TO BE CREATED")
+time.sleep(10)
 
-# upload transactions and removed transactions to associated BQ tables
-plaid_client.upload_transactions_df_to_bq(transactions_df)
-plaid_client.upload_removed_df_to_bq(removed_df)
+# Run get_transactions() to store added/modified transactions in transactions_df and removed transactions in removed_df
+for i, row in latest_cursors_df.iterrows():
+
+    transactions_df, removed_df = plaid_client.get_transactions(
+        access_token=row["access_token"],
+        item_id=row["item_id"],
+        next_cursor=row["next_cursor"],
+        offset_days=offset_days,
+    )
+
+    # upload transactions and removed transactions to associated BQ tables
+    plaid_client.upload_transactions_df_to_bq(transactions_df)
+    plaid_client.upload_removed_df_to_bq(removed_df)
+
+# Copy temp_cursors to plaid_cursors_YYYYMMDD
+plaid_client.copy_temp_cursors_to_cursors_bq_table(offset_days=offset_days, write_disposition="WRITE_TRUNCATE")
